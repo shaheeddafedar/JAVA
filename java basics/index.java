10281a63d43edaf81a445262fd509c33ce399927
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-09 17:05:54 +0530

    start with the java

--- java basics/index.java
+++ java basics/index.java
@@ -1,5 +1,9 @@
+
+
 public class index {
-    public static void main(String [] args){
-        System.out.println("hello");
-    }
+public static void main(String[] args) {
+   int num1 = 8;
+   int num2 =5;
+   System.out.println((float) (num1/num2) );
 }
+} 
