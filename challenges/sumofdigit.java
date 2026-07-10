200f16b4e838fe08b6addcafcaed3b072e3ae0f8
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-10 22:07:05 +0530

    solving challenges  sum of digit

--- challenges/sumofdigit.java
+++ challenges/sumofdigit.java
@@ -1,13 +1,24 @@
+
+import java.util.Scanner;
+
 public class sumofdigit {
     public static void main(String[] args) {
-        int num =12345;
+      Scanner input = new Scanner(System.in);
+      int num = input.nextInt();
+      int result = sumOfDigit(num);
+      System.out.println("Sum of Digit is "+ result);
+
+    }
+
+    public static int sumOfDigit(int num){
         int digit=0;
-        int sumofdigit=0;
+        int sum=0;
         while (num>0) { 
              digit = num%10;
-            sumofdigit+=digit;
+            sum+=digit;
             num=num/10;
         }
-        System.out.println(sumofdigit);
+        System.out.println(sum);
+     return  sum;
     }
 }
