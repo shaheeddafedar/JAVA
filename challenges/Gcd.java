4c9cbcf65b0838864c2166d32c53543783a90073
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-11 15:45:02 +0530

    solving challenges GCD

---
+++ challenges/Gcd.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Gcd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number one ");
+        int num1 = input.nextInt();
+        System.out.print("Enter Number two ");
+        int num2 = input.nextInt();
+        int GCD = GCD(num1, num2);
+        System.out.println("GCD is " + GCD);
+
+    }
+
+    public static int GCD(int num1, int num2) {
+        int i = 1;
+        int limit = 0;
+        int GCD = 0;
+
+        if (num1 < num2) {
+            limit = num1;
+        } else {
+            limit = num2;
+        }
+
+        while (i <= limit) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                GCD = i;
+            }
+            i++;
+        }
+        return GCD;
+    }
+
+}
