e716615479e5eb268ef9da79554346aa59e321b4
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-11 14:52:21 +0530

    solving challenges  LCM

--- challenges/LCM.java
+++ challenges/LCM.java
@@ -1,7 +1,27 @@
+
+import java.util.Scanner;
+
 public class LCM {
  public static void main(String[] args) {
-    
- }   
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter Number one ");
+    int num1 = input.nextInt();
+    System.out.print("Enter Number two ");
+    int num2 = input.nextInt();
+    int LCM = LCM(num1, num2);
+   System.out.println("LCM is "+ LCM);
+}
+
+public static int LCM(int num1, int num2){
+    int i =1;
+    while (true) { 
+        if (i%num1==0 && i%num2 ==0) {
+            return i;
+
+        }
+        i++;
+    }
+}
 }
 
 
