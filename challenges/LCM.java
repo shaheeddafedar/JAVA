f828e798ec2e4fa13f382b82c38b5fcd34051a04
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-11 14:42:33 +0530

    solving challenges  LCM

---
+++ challenges/LCM.java
@@ -0,0 +1,28 @@
+public class LCM {
+ public static void main(String[] args) {
+    
+ }   
+}
+
+
+
+
+// Method 1
+//  int a =2;
+//      int b =3;
+//     int i =1;
+//     while (true) { 
+//         if (i%a==0 && i%b==0) {
+//             System.out.println("lcm " +i);
+//             break;
+//         }
+//         i++;
+//     }
+    //    method 2
+//     int max = a*b;
+//     for (int i = 1; i <=max; i++){
+//         if (i%a==0 && i%b==0) {
+//             System.out.println("LCM is "+ i);
+//             break;
+//         }
+//     }
