409c05c2554f48a1f0c78ab8671a7651c2cf1750
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-12 21:45:39 +0530

    solving challenges reveri palidrome

--- challenges/palindrome.java
+++ challenges/palindrome.java
@@ -1,36 +1,43 @@
+import java.util.Scanner;
+
 public class palindrome {
- public static void main(String[] args) {
-    int num = 123;
-    int copynum =num;
-    int checkplaindrome =0;
-    int digit =0;
-    while (num>0) { 
-        digit=num%10;
-        checkplaindrome=checkplaindrome*10+digit;
-        num/=10;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number  ");
+        int num = input.nextInt();
+        int copynum = num;
+        int checkplaindrome = palindrome(num);
+        if ( checkplaindrome == copynum) {
+            System.out.println("number is plaindrome");
+        } else {
+            System.out.println("Number is not a palindrome ");
+        }
     }
 
-    if (checkplaindrome==copynum) {
-        System.out.println("number is plaindrome");
-    } else {
-        System.out.println("Number is not a palindrome ");
+    public static int palindrome(int num) {
+        int checkplaindrome = 0;
+        int digit = 0;
+        while (num > 0) {
+            digit = num % 10;
+            checkplaindrome = checkplaindrome * 10 + digit;
+            num /= 10;
+        }
+        return checkplaindrome;
     }
- }   
 }
 
+// int num = 123;
+// int copynum =num;
+// int checkplaindrome =0;
+// int digit =0;
+// while (num>0) {
+// digit=num%10;
+// checkplaindrome=checkplaindrome*10+digit;
+// num/=10;
+// }
 
-    // int num = 123;
-    // int copynum =num;
-    // int checkplaindrome =0;
-    // int digit =0;
-    // while (num>0) { 
-    //     digit=num%10;
-    //     checkplaindrome=checkplaindrome*10+digit;
-    //     num/=10;
-    // }
-
-    // if (checkplaindrome==copynum) {
-    //     System.out.println("number is plaindrome");
-    // } else {
-    //     System.out.println("Number is not a palindrome ");
-    // }
+// if (checkplaindrome==copynum) {
+// System.out.println("number is plaindrome");
+// } else {
+// System.out.println("Number is not a palindrome ");
+// }
