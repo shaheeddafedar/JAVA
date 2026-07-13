d0a97a114fd4f56a1de8c60014ae902005ea4ddf
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-13 15:30:20 +0530

    solving challenges arrays 1 sum and average

---
+++ arrayschallenges/Sumandaverga_array.java
@@ -0,0 +1,20 @@
+// Create a program to find the sum and average of all elements in an array.
+
+package arrayschallenges;
+
+public class Sumandaverga_array {
+    public static void main(String[] args) {
+       int []values ={1,2,3,4,5,6,7,8,9,10};
+     
+}
+}
+
+
+    //       int sum=0;
+    //       float average=0;     
+    //    for (int i = 0; i <values.length; i++) {
+    //        sum+=values[i];
+    //    }
+    //     average= (float) sum/values.length;
+    //    System.out.println("sum is "+ sum);
+     //    System.out.println("average is "+average);
