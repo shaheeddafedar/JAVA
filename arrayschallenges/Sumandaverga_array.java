ce396a50bbb3869908638a5b225b72e873a14622
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-13 15:54:16 +0530

    solving challenges arrays 1 sum and average

--- arrayschallenges/Sumandaverga_array.java
+++ arrayschallenges/Sumandaverga_array.java
@@ -2,12 +2,51 @@
 
 package arrayschallenges;
 
+import java.util.Scanner;
+
 public class Sumandaverga_array {
     public static void main(String[] args) {
-       int []values ={1,2,3,4,5,6,7,8,9,10};
-     
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Lenght of array  ");
+        int size = input.nextInt();
+        int[] values = new int[size];
+
+        inputarray(values, input);
+        int sum = sum(values);
+        float average = avergae(values, sum);
+
+        System.out.println("Sum of Array is " + sum);
+        System.out.println("Average of Array is " + average);
+
+    }
+
+    public static void inputarray(int[] values, Scanner input) {
+        for (int i = 0; i < values.length; i++) {
+            System.out.print("Enter element " + i + ": ");
+            values[i] = input.nextInt();
+        }
+    }
+
+    public static int sum(int[] values) {
+        int sum = 0;
+        for (int i = 0; i < values.length; i++) {
+            sum += values[i];
+        }
+        return sum;
+    }
+
+    public static float avergae(int[] values, int sum) {
+        float avergae;
+        avergae = (float) sum / values.length;
+        return avergae;
+    }
+
 }
-}
+
+
+
+
+
 
 
     //       int sum=0;
@@ -18,3 +57,4 @@ public class Sumandaverga_array {
     //     average= (float) sum/values.length;
     //    System.out.println("sum is "+ sum);
      //    System.out.println("average is "+average);
+
