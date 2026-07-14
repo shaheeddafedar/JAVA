3e1ee05bd81f4de7259161b723debd3bccad8700
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-15 00:33:43 +0530

    upadting Arrayutility for 2d array

--- arrayschallenges/SumAndAverga.java
+++ arrayschallenges/SumAndAverga.java
@@ -2,5 +2,16 @@
 package arrayschallenges;
 
 public class SumAndAverga {
-    
+    public static void main(String[] args) {
+        int [][]array=ArrayUtility.input2DArray();
+    }
+    public int sum(int []array){
+        int sum =0;
+        for (int i = 0; i <array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                sum+=sum;
+            }
+        }
+        return sum;
+    }
 }
