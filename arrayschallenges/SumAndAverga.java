2d6da46983b4a6ac761d11e35ec847019e31fc97
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-15 16:00:12 +0530

    find the sum of two diagonal

--- arrayschallenges/SumAndAverga.java
+++ arrayschallenges/SumAndAverga.java
@@ -3,15 +3,34 @@ package arrayschallenges;
 
 public class SumAndAverga {
     public static void main(String[] args) {
-        int [][]array=ArrayUtility.input2DArray();
+        int[][] array = ArrayUtility.input2DArray();
+
+        int sum = sumarray(array);
+        System.out.println("Sum of Array is " + sum);
+
+        float average = averga(array, sum);
+        System.out.println("Average of Array is " + average);
     }
-    public int sum(int []array){
-        int sum =0;
-        for (int i = 0; i <array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                sum+=sum;
+
+    public static int sumarray(int[][] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                sum += array[i][j];
             }
         }
         return sum;
+    }
+
+    public static float averga(int[][] array, int sum) {
+        int length = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                length = length + 1;
+            }
+        }
+
+        float average = sum / length;
+        return average;
     }
 }
---
+++ arrayschallenges/Sumofdiagonal.java
@@ -0,0 +1,22 @@
+// 50. Create a program to find the sum of two diagonal elements.
+package arrayschallenges;
+
+public class Sumofdiagonal {
+    public static void main(String[] args) {
+         int[][] array = ArrayUtility.input2DArray();
+         int sum = sum(array);
+         System.out.println(sum);
+    }
+    public static int sum(int [][]array){
+        int lenght=array.length;
+        int sum =0;
+        for (int i = 0; i <lenght; i++) {
+            for (int j = 0; j <lenght; j++) {
+                if (i ==j) {
+                    sum=sum+array[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+}
