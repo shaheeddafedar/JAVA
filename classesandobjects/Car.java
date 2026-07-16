89428b275837ec5e04649171e8ec9c90a23fc692
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-16 16:53:41 +0530

    classes and objects

--- classesandobjects/Car.java
+++ classesandobjects/Car.java
@@ -8,8 +8,17 @@ public class Car {
     int noOfSheets;
 
     public void drive(){
+        if (currentFuelInListers==0) {
+            System.out.println("Car is Out of Fuel");
+        } else if (currentFuelInListers<=5) {
+            System.out.println("Car is driving");
+            System.out.println("Car is Under the Reserve");
+            currentFuelInListers--;
+        } else {
         System.out.println("Car is driving");
         currentFuelInListers--;
+        }
+
     }
     public  void  addFuel(float fuel){
         currentFuelInListers+=fuel;
--- classesandobjects/Driver.java
+++ classesandobjects/Driver.java
@@ -3,6 +3,7 @@ package classesandobjects;
 public class Driver {
  public static void main(String[] args) {
      Car myCar = new Car();
+     myCar.addFuel(6);
      myCar.drive();
  }   
 }
