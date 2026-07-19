70a6291dac5be190325ae5d2d4494eee1d8acda8
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-20 00:24:53 +0530

    basic challenge

--- controlstatement/mintwo.java
+++ controlstatement/mintwo.java
@@ -3,16 +3,19 @@ package controlstatement;
 import java.util.Scanner;
 
 public class mintwo {
+    public int minternary(int num1,int num2){
+        return num1 < num2 ? num1 : num2;
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter Number one");
+        System.out.print("Enter Number one");
         int num1 = input.nextInt();
-        System.out.println("Enter Number two");
+        System.out.print("Enter Number two");
         int num2 = input.nextInt();
         input.close();
+         mintwo min = new mintwo();   
+        int minimum = min.minternary(num1, num2);
 
-        int minimum = num1 < num2 ? num1 : num2;
-
-        System.out.println("Minimum number is " + minimum);
+        System.out.print("Minimum number is " + minimum);
     }
 }
