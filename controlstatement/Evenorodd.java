f8e9f75fbb919ba99d8cdef85889725a40f53b0a
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-20 00:27:20 +0530

    basic challenge

--- controlstatement/Evenorodd.java
+++ controlstatement/Evenorodd.java
@@ -5,14 +5,20 @@ package controlstatement;
 import java.util.Scanner;
 
 public class Evenorodd {
+
+    public void checknum(int num) {
+        String result = (num % 2 == 0) ? "Number is even" : "Number is odd";
+        System.out.println(result);
+
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Number ");
         int num = input.nextInt();
         input.close();
+        Evenorodd check = new Evenorodd();
+        check.checknum(num);
 
-        String result = (num % 2 == 0) ? "Number is even" : "Number is odd";
-
-        System.out.println(result);
     }
 }
