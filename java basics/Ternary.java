43a3e9cd2fb36463a6af9072585f7602d62770e2
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-19 22:29:03 +0530

    basic

---
+++ java basics/Ternary.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Ternary {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+       int greatest= num1>num2?num1:num2;
+       System.out.println(greatest);
+    }
+}
