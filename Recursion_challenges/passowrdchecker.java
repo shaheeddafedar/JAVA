8185f2403270a1aef8ece989902f1f78718a21d6
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-21 15:42:01 +0530

    password checker

--- Recursion_challenges/passowrdchecker.java
+++ Recursion_challenges/passowrdchecker.java
@@ -2,52 +2,41 @@
 // password is entered.
 
 package Recursion_challenges;
+
 import java.util.Scanner;
 
+public class passowrdchecker {
 
-// package Recursion_challenges;
+    public boolean check_passowrd(String password) {
+        return password.length() > 6;
+    }
 
-// import java.util.Scanner;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        passowrdchecker check = new passowrdchecker();
+        String Password;
+        do {
+            System.out.println("Enter your Password");
+            Password = input.next();
+        } while (!check.check_passowrd(Password));
 
-// public class passowrdchecker {
-
-//     public void check_passowrd(String password){
-//                 Scanner input = new Scanner(System.in);
-//          String verfiypassowrd;
-//         do {
-//             System.out.println("Enter your password again");
-//             verfiypassowrd=input.next();
-//         } while (password==verfiypassowrd);
-//        System.out.println("The Verifyed Passowrd is correct"); 
-//     }
-
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
-//         System.out.println("Enter your Password");
-//         String Password =input.next();
-
-//         passowrdchecker check = new passowrdchecker();
-
-//         check.check_passowrd(Password);
-//     }
-
-    
-// }
-
-
- public class passowrdchecker {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter your passowrd");
-    String Passowrd = input.next();
-    String verifypassowrd;
-
-    do {
-        System.out.println("Enter your Passord again for verify");
-        verifypassowrd=input.next();
-    } while (!Passowrd.equals(verifypassowrd));
-    System.out.println("Enterd Passsword is correct");
-  }
+        System.out.println("Your Passowrd is Valid");
+    }
 
 }
 
+// public class passowrdchecker {
+// public static void main(String[] args) {
+// Scanner input = new Scanner(System.in);
+// System.out.println("Enter your passowrd");
+// String Passowrd = input.next();
+// String verifypassowrd;
+
+// do {
+// System.out.println("Enter your Passord again for verify");
+// verifypassowrd=input.next();
+// } while (!Passowrd.equals(verifypassowrd));
+// System.out.println("Enterd Passsword is correct");
+// }
+
+// }
