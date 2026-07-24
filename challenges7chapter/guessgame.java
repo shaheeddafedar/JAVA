bbeaf65273bdf922f2842b12cefcb742a2d84fc9
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-24 22:49:00 +0530

    correcting the erroe

--- challenges7chapter/guessgame.java
+++ challenges7chapter/guessgame.java
@@ -1,4 +1,4 @@
-// 74. Create a number guessing game where the program selects a // random number, and the user has to guess it.
+// 74. Create a number guessing game where the program selects a random number, and the user has to guess it.
 package challenges7chapter;
 import java.util.Scanner;
 
