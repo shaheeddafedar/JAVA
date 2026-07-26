5d120d6714df95d0665bad9356d08b8023b3383b
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-26 20:49:40 +0530

    Solving Encapsulation Problem

--- Inheritance_challenge/Person.java
+++ Inheritance_challenge/Person.java
@@ -22,21 +22,16 @@ public class Person {
 
     @Override
     public boolean equals(Object obj) {
+
         if (this == obj)
             return true;
-        if (obj == null)
+
+        if (!(obj instanceof Person))
             return false;
-        if (getClass() != obj.getClass())
-            return false;
+
         Person other = (Person) obj;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (age != other.age)
-            return false;
-        return true;
+
+        return age == other.age && name.equals(other.name);
     }
 
     public static void main(String[] args) {
@@ -47,5 +42,5 @@ public class Person {
         System.out.println(p1.equals(p2));
         System.out.println(p1.equals(p3));
     }
-    
+
 }
