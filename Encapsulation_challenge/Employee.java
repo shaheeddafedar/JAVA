8b82d34ab43dae0a462344b341dfbf83130bce9f
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-26 17:08:58 +0530

    Solving Encapsulation Problem

--- Encapsulation_challenge/Employee.java
+++ Encapsulation_challenge/Employee.java
@@ -52,5 +52,41 @@ public class Employee {
         System.out.println("Salary "+salary);
     }
 
+
+     @Override
+     public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + age;
+        long temp;
+        temp = Double.doubleToLongBits(salary);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+     }
+
+
+     @Override
+     public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Employee other = (Employee) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (age != other.age)
+            return false;
+        if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
+            return false;
+        return true;
+     }
+    
+
 }
 
