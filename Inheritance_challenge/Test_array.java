3ab41b59ef6f774e6280fe9fbf795cddf04542d3
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-26 22:01:52 +0530

    Solving Encapsulation Problem

--- Inheritance_challenge/Test_array.java
+++ Inheritance_challenge/Test_array.java
@@ -2,7 +2,6 @@ package Inheritance_challenge;
 
 public class Test_array {
     public static void main(String[] args) {
-        ArrayOperations arr = new ArrayOperations();
         ArrayOperations.Statistics st = new ArrayOperations.Statistics();
         st.mean();
         st.median();
