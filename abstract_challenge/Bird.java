9577f1e2a33be7079b875c4b6dc1fbf11d78a335
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-27 23:13:03 +0530

    Solving abstract  Problem

--- abstract_challenge/Bird.java
+++ abstract_challenge/Bird.java
@@ -1,9 +1,25 @@
 package abstract_challenge;
 
 public abstract class Bird implements Flyable {
+
+    private String breed;
+
+    public Bird(String breed) {
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
     @Override
     public void fly() {
         System.out.println("Many birds can fly, except for some, such as ostriches and chickens");
     }
+
 }
 
--- abstract_challenge/Eagle.java
+++ abstract_challenge/Eagle.java
@@ -1,12 +1,16 @@
 package abstract_challenge;
 
 public class Eagle extends Bird {
+    public Eagle(){
+        super("booted eagles");
+    }
     @Override
     public void fly() {
-        super.fly();
+        System.out.println("Eagle is flying which is a  Breed of "+getBreed());
     }
+
     public static void main(String[] args) {
         Eagle e = new Eagle();
-        System.out.println(e);
+        e.fly();
     }
 }
