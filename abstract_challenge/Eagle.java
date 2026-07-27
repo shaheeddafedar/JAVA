8d748daf3b49a343396f03e60110932795af428f
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-27 23:18:43 +0530

    Solving abstract  Problem

--- abstract_challenge/Eagle.java
+++ abstract_challenge/Eagle.java
@@ -4,13 +4,14 @@ public class Eagle extends Bird {
     public Eagle(){
         super("booted eagles");
     }
-    
+
     @Override
     public void fly() {
         System.out.println("Eagle is flying which is a  Breed of "+getBreed());
     }
 
     public static void main(String[] args) {
+        
         Eagle e = new Eagle();
         e.fly();
     }
