c4a21fce1625bede128593c310177c58962c9377
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-27 22:31:52 +0530

    Solving abstract  Problem

--- abstract_challenge/Square.java
+++ abstract_challenge/Square.java
@@ -1,5 +1,26 @@
 package abstract_challenge;
 
-public class Square {
-    
+public class Square extends Shape {
+    protected int side;
+
+    public Square(int side){
+        this.side=side;
+    }
+    @Override
+    public String toString() {
+        return "Cricle [ Area=" + area + "]";
+    }
+   
+    double area =0;
+    @Override
+    public void calculateArea() {
+        area = Math.pow(side, 2);
+        
+    }
+
+    public static void main(String[] args) {
+        Square s = new Square(8);
+        s.calculateArea();
+        System.out.println(s);
+    }
 }
