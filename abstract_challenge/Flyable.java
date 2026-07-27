6a0a03f9d351a878df4ae4c798c94d0ed2b046ba
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-27 23:16:36 +0530

    Solving abstract  Problem

--- abstract_challenge/Flyable.java
+++ abstract_challenge/Flyable.java
@@ -1,3 +1,5 @@
+// 84. Create an interface Flyable with an abstract method fly().Create an abstract class Bird that implements Flyable. Implement a subclass Eagle that extends Bird. Provide an implementation for the fly() method
+
 package abstract_challenge;
 
 public interface Flyable {
