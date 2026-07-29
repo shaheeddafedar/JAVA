97a61b6cb4d04c4de9d8a32e36506fc9fb6dec37
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-07-29 22:44:43 +0530

    Solving method overloading

--- Chapter9_challenge/Calculator.java
+++ Chapter9_challenge/Calculator.java
@@ -18,8 +18,7 @@ public class Calculator {
 
     public static void main(String[] args) {
         System.out.println(add(10, 20));
-        System.out.println(add(25.6, 35.6));
-        
+        System.out.println(add(25.6, 35.6));  
         System.out.println(add(10, 20, 30));
     }
 
