f40832b3fe7f6eab65c81ffab2cd6bc539dd3d4a
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-02 01:53:18 +0530

    Varible Argumnet challenge 89

--- Collections/challenge89.java
+++ Collections/challenge89.java
@@ -1,22 +1,29 @@
-// 89. Write a method concatenate Strings that takes variable arguments 
-// of String type and concatenates them into a single string.
+// 89. Write a method concatenate Strings that takes variable arguments  of String type and concatenates them into a single string.
 package Collections;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class challenge89 {
     public static void main(String[] args) {
-      Scanner input = new Scanner(System.in);
-      String str = input.nextLine();
-     String resut = concatenateStrings(str);
-     System.out.print(resut);
-    }
-    public static String  concatenateStrings(String... str){
-      String combined= "";
-        for (String string : str) {
-            combined += string;
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many String u want to Enter ?...");
+        int n = input.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = input.nextLine();
         }
-        return combined;
+        String resut = concatenateStrings(str);
+        System.out.print(resut);
+        input.close();
     }
+
+    public static String concatenateStrings(String... str) {
+        StringBuilder combined = new StringBuilder();
+        for (String string : str) {
+            combined.append(string).append(" ");
+        }
+        return combined.toString();
+
+    }
+
 }
