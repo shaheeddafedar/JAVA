9ffe36b4fea2dfb4caceb7469708c336e5c496aa
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-02 19:24:52 +0530

    Sorting list  challenge 93

--- Collections/Challenge91.java
+++ Collections/Challenge91.java
@@ -2,7 +2,6 @@
 
 package Collections;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
---
+++ Collections/Challenge93.java
@@ -0,0 +1,22 @@
+// 93 Create a program that reverses the elements of a List and prints the reversed list.
+package Collections;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Challenge93 {
+    public static void main(String[] args) {
+        List<Integer> list = Utility.inputList();
+        // Collections.reverse(list);
+        reverse(list);
+        System.out.println(list);
+    }
+
+    public static void reverse(List<Integer> list) {
+        for (int i = 0; i < list.size() / 2; i++) {
+            int temp = list.get(i);
+            list.set(i, list.get(list.size() - 1 - i));
+            list.set(list.size() - 1 - i, temp);
+        }
+    }
+}
