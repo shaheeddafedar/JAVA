0b6cd3489c887396785c83b31fdfc144984ecf46
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-02 17:40:53 +0530

    List Frequency  challenge 91

--- Collections/Challenge90.java
+++ Collections/Challenge90.java
@@ -2,9 +2,7 @@
 package Collections;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class Challenge90 {
---
+++ Collections/Challenge91.java
@@ -0,0 +1,29 @@
+// 91. Use the Collections class to count the frequency of a particular element in an ArrayList.
+
+package Collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Challenge91 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many numbers? ");
+        int n = input.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            list.add(input.nextInt());
+        }
+        System.out.println("Eneter the element to find frequency");
+        int f = input.nextInt();
+
+        int count = Collections.frequency(list, f);
+        System.out.printf("Frequency of element %d: %d", f, count);
+
+        input.close();
+    }
+}
