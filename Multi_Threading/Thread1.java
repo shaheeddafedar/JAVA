852a810d35b8f51e25cb0b93ed29536d5ac14da8
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-06 01:11:36 +0530

    Complete 100 Problems in JAVA

---
+++ Multi_Threading/Thread1.java
@@ -0,0 +1,21 @@
+package Multi_Threading;
+
+public class Thread1 extends Thread {
+   private int num;
+
+   public Thread1(int num) {
+    this.num = num;
+   }
+
+   public void run(){
+    System.out.println("The Thread "+num+" " +Thread.currentThread().getName());
+    try {
+        Thread.sleep(2000);
+    } catch (InterruptedException e) {
+        System.out.println("Exception during Thread "+num);
+    }
+   }
+
+
+
+}
---
+++ Multi_Threading/Thread2.java
@@ -0,0 +1,19 @@
+package Multi_Threading;
+
+public class Thread2 extends Thread {
+     private int num;
+
+   public Thread2(int num) {
+    this.num = num;
+   }
+
+   public void run(){
+    System.out.println("The Thread "+num+" " +Thread.currentThread().getName());
+    try {
+        Thread.sleep(2000);
+    } catch (InterruptedException e) {
+        System.out.println("Exception during Thread "+num);
+    }
+   }
+
+}
