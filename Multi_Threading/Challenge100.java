7fd437c9b61ea934fa97af08e7b1dff2d07aa375
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-06 01:02:20 +0530

    Complete 100 Problems in JAVA

--- Multi_Threading/Challenge100.java
+++ Multi_Threading/Challenge100.java
@@ -5,16 +5,20 @@ package Multi_Threading;
 public class Challenge100 {
     public static void main(String[] args) {
         ThreadState t = new ThreadState();
-        t.start();
-        System.out.println("Creating Thread" + t.getState());
+        System.out.println("Creating Thread");
+        System.out.println(t.getState());
 
         t.start();
+        System.out.println("Started Thread");
+        System.out.println(t.getState());
 
         try {
             t.join();
         } catch (InterruptedException e) {
             System.out.println("Handling Before Termination");
         }
-        System.out.println("Thread Finished" + t.getState());
+        System.out.println("Thread Finished");
+        System.out.println(t.getState());
+
     }
 }
