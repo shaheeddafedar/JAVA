5cf5c289c0b56eb28c537bc7a6ddc2e6eb732048
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-07 22:20:38 +0530

    Solving Multi_Threading  Problems challenge number 102

--- Multi_Threading/Challenge102.java
+++ Multi_Threading/Challenge102.java
@@ -1,6 +1,6 @@
 // 102. Simulate a traffic signal using threads. Create three threads representing three signals: RED, YELLOW, and GREEN. Each signal should be on for a certain time, then switch to the next signal in order. Use sleep for timing and synchronize to make sure only one signal is active at a time.
-
 package Multi_Threading;
+
 public class Challenge102 {
     public static void main(String[] args) throws InterruptedException {
         Signal RED = new Signal(Signalcolor.RED);
