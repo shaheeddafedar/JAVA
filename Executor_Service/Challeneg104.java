81f3dd60260988ac3aab411a182f7518feb4e0e5
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-09 00:40:03 +0530

    Solving Executor_Service  Problems challenge number 104

--- Executor_Service/Challeneg104.java
+++ Executor_Service/Challeneg104.java
@@ -2,7 +2,6 @@
 // to this executor, where each task should print the current thread's name and sleep for a random time between 1 and 5 seconds. 
 // Finally, shut down the executor and handle proper termination using awaitTermination.
 
-
 package Executor_Service;
 
 import java.util.concurrent.ExecutorService;
@@ -12,21 +11,21 @@ import java.util.concurrent.TimeUnit;
 public class Challeneg104 {
     public static void main(String[] args) {
 
-        ExecutorService execute  =  Executors.newFixedThreadPool(3);
-        
-        for (int i = 1; i <=10; i++) {
+        ExecutorService execute = Executors.newFixedThreadPool(3);
+
+        for (int i = 1; i <= 10; i++) {
             Thread104 t1 = new Thread104();
-                    execute.submit(t1);
+            execute.submit(t1);
         }
-       execute.shutdown();
-   try {
-      if (!execute.awaitTermination(20, TimeUnit.SECONDS)) {
-        execute.shutdownNow();
+        execute.shutdown();
+        try {
+            if (!execute.awaitTermination(20, TimeUnit.SECONDS)) {
+                execute.shutdownNow();
                 System.out.println("Forecfully Shutdown");
 
-     }
-   } catch (Exception e) {
-  System.out.println("Exception during Shutdown "+e);
-}
+            }
+        } catch (Exception e) {
+            System.out.println("Exception during Shutdown " + e);
+        }
     }
 }
