bb780f150fc0f8889f16c7a4300e0bf4ec5ebb1f
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-10 00:22:14 +0530

    Solving chat problem

--- Executor_Service/Chat_challenges/problem1.java
+++ Executor_Service/Chat_challenges/problem1.java
@@ -2,18 +2,24 @@ package Executor_Service.Chat_challenges;
 
 public class problem1 extends Thread {
 
-    public void run(){
-        for (int i = 1; i <=5; i++) {
-            if (i%i==0) {
+    public void run() {
+        for (int i = 2; i <= 5; i++) {
+            int count = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
                 System.out.println(i);
             }
         }
     }
+
     public static void main(String[] args) {
         problem1 p1 = new problem1();
-
         p1.start();
     }
-    
-
 }
