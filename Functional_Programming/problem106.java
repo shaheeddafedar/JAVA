fab69ba0b61e2d31d256d74bce33c4f0e209a782
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-11 00:53:37 +0530

    Solving lamda problem 106

--- Functional_Programming/problem106.java
+++ Functional_Programming/problem106.java
@@ -22,3 +22,22 @@ int multiplication(int a, int b);
 
     }
 }
+
+
+
+// package Functional_Programming;
+
+// import java.util.function.BinaryOperator;
+
+
+// public class problem106 {
+//     interface multip {
+// int multiplication(int a, int b);
+// }
+//     public static void main(String[] args) {
+//      BinaryOperator<Integer> multi = (a,b)->a*b;  
+//      int result = multi.apply(10, 10);
+//      System.out.println("Result is "+result);
+
+//     }
+// }
