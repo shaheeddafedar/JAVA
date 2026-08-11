81f24b1a7883a176d923bfd18353ad36c10056fd
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-12 01:30:31 +0530

    Solving Functional Interface  problem 113

---
+++ Functional_Programming/Problem114.java
@@ -0,0 +1,41 @@
+package Functional_Programming;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Problem114 {
+    public int salary;
+    public String name;
+
+public Problem114(int salary, String name) {
+    this.salary = salary;
+    this.name = name;
+ }
+ 
+ @Override
+ public String toString() {
+    return "Problem114 [salary=" + salary + ", name=" + name + "]";
+ }
+ public static void main(String[] args) {
+    List<Problem114> employees = new ArrayList<>();
+    employees.add(new Problem114(25000, "kavin"));
+    employees.add(new Problem114(55000, "roman"));
+    employees.add(new Problem114(5000, "seth"));
+    employees.add(new Problem114(45000, "Brock"));
+
+    Comparator<Problem114> salarysort = new Comparator<Problem114>() {
+        public int compare( Problem114 p1 ,Problem114 p2){
+        return Integer.compare(p1.salary, p2.salary);
+        }
+    };
+
+    Collections.sort(employees,salarysort);
+    System.out.println("Sorted using Salary : "+ employees);
+ }
+
+
+ 
+}
+
---
+++ Functional_Programming/problem113.java
@@ -0,0 +1,15 @@
+// 113.Given an array of integers, create a stream, use the distinct 
+// operation to remove duplicates, and collect the result into 
+// a new list
+
+package Functional_Programming;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class problem113 {
+    public static void main(String[] args) {
+        List<Integer> number = List.of(1,2,3,4,5,2,4,6,7);
+       List<Integer> disntincnumber = number.stream().distinct().collect(Collectors.toList());
+       System.out.println(disntincnumber);
+    }
+}
