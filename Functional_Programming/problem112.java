2dd733a3b8106fbc210975a74c33ec4f386d9767
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-12 00:27:54 +0530

    Solving Functional Interface  problem 112

---
+++ Functional_Programming/problem112.java
@@ -0,0 +1,26 @@
+// 112.Write a function that accepts a string and returns an 
+// Optional<String>. If the string is empty or null, return an 
+// empty Optional, otherwise, return an Optional containing the 
+// uppercase version of the string
+
+package Functional_Programming;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+public class problem112 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Name :");
+        String name = input.nextLine();
+        input.close();
+        System.out.println(Testotpional(name));
+    }
+
+    public static Optional<String> Testotpional(String name) {
+        if(name == null || name.isEmpty()){
+            return null;
+        }
+        return Optional.of(name.toUpperCase());
+    }
+}
