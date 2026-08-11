72799565967d22feca4e451713ff38fe00ea7c9b
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-11 23:47:26 +0530

    Solving Functional Interface  problem 111

--- Functional_Programming/Problem111.java
+++ Functional_Programming/Problem111.java
@@ -5,6 +5,7 @@
 package Functional_Programming;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Problem111 {
     interface factorial {
@@ -12,27 +13,58 @@ public class Problem111 {
     }
 
     public static void main(String[] args) {
-        // Structural Programming
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the  number you want to check for prime  :");
-        int num1 = input.nextInt();
+        System.out.print("Enter the Factrioal Number  :");
+        int num = input.nextInt();
         input.close();
-        int fact = 1;
-        for (int i = 2; i <= num1; i++) {
-            fact = fact * i;
-        }
-        System.out.println("Factioral using Structural Programming " + fact);
+        System.out.println("Factrioal Using  Structural programming "+Fact(num));
 
         // functional programming
-        factorial facto = (num) -> {
-            int facties = 1;
-            for (int i = 2; i <= num; i++) {
-                facties = facties * i;
-            }
-            return facties;
-        };
-        System.out.println("Factioral using functional programming " + facto.fact(num1));
+        System.out.print("Factrioal Using  Functional programming ");
+       IntStream.rangeClosed(2, num)
+       .reduce((a,b)->a*b).ifPresent(System.out::print);
 
     }
 
+    // Structural Programming
+    public static int Fact(int num) {
+        int fact = 1;
+        for (int i = 2; i <= num; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
 }
+
+// public class Problem111 {
+// interface factorial {
+// int fact(int num);
+// }
+
+// public static void main(String[] args) {
+// // Structural Programming
+// Scanner input = new Scanner(System.in);
+// System.out.print("Enter the number you want to check for prime :");
+// int num1 = input.nextInt();
+// input.close();
+// int fact = 1;
+// for (int i = 2; i <= num1; i++) {
+// fact = fact * i;
+// }
+// System.out.println("Factioral using Structural Programming " + fact);
+
+// // functional programming
+// factorial facto = (num) -> {
+// int facties = 1;
+// for (int i = 2; i <= num; i++) {
+// facties = facties * i;
+// }
+// return facties;
+// };
+// System.out.println("Factioral using functional programming " +
+// facto.fact(num1));
+
+// }
+
+// }
