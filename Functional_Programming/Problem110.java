617f4665afd230386d8cb9b62032ab6dfb967ed7
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-11 20:44:03 +0530

    Solving Functional Interface  problem 110

--- Functional_Programming/Problem110.java
+++ Functional_Programming/Problem110.java
@@ -1,37 +1,30 @@
-// 110.Create your own functional interface with a single abstract 
-// method that accepts an integer and returns a boolean. 
-// Implement it using a lambda that checks if the number is 
-// prime.
+// 110.Create your own functional interface with a single abstract  method that accepts an integer and returns a boolean.  Implement it using a lambda that checks if the number is  prime.
 package Functional_Programming;
 
 import java.util.Scanner;
 
 public class Problem110 {
-     interface intergernumber{
-   boolean bollennumber(int num);
-     }
+    interface Cadinate {
+        boolean isprime(int num);
+    }
+
     public static void main(String[] args) {
-      intergernumber inte = (num)->{
-        int count =0;
-        for (int i = 1; i <=num; i++) {
-            if (num%i==0) {
-                count++;
+        Cadinate isprime = (num) -> {
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    return false;
+                }
             }
-        }
-        if (count==2) {
             return true;
-        } else{
-            return false;
+        };
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the  number you want to check for prime  :");
+        int a = input.nextInt();
+        input.close();
+        if (isprime.isprime(a)) {
+            System.out.println("The Given Number Is a Prime Number");
+        } else {
+            System.out.println("The Given Number Is Not  a Prime Number");
         }
-      };
-     Scanner input = new Scanner(System.in);
-      System.out.print("Enter the  number you want to check for prime  :");
-      int a = input.nextInt();
-      System.out.println(inte.bollennumber(a)); 
-      if (inte.bollennumber(a)) {
-        System.out.println("The Given Number Is a Prime Number");
-      } else {
-                System.out.println("The Given Number Is Not  a Prime Number");
-      }
     }
 }
