bfe5f0af109882e81b0fdeee54988a439be10663
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-25 00:12:24 +0530

    Armstrong

--- challenges/Armstrong.java
+++ challenges/Armstrong.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Armstrong {
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Number  ");
